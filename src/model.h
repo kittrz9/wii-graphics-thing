b05b25a3e6973db2254c197cbb4a7807c390fe4f
kittrz <kittrz@kittrz.gay>
2024-10-25 16:25:05 -0500

    kinda get lighting to work

--- src/model.h
+++ src/model.h
@@ -5,7 +5,11 @@
 
 extern u32 vertCount;
 extern u32 indexCount;
+extern u32 normalCount;
+extern u32 normalIndexCount;
 extern f32 vertPositions[];
 extern u16 vertIndices[];
+extern f32 normals[];
+extern u16 normalIndices[];
 
 #endif // MODEL_H
